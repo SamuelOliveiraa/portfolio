0a1ad3d889770b66caa19876df76fa2f698d3a97
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2021-09-23 20:03:19 -0300

    Adicionei os arquivos e estou terminando

---
+++ Assets/script.js
@@ -0,0 +1,20 @@
+//ESCONDE E MOSTRA AS PERGUNTAS DA SEÇÃO AKS    
+$('#tit1').click(function(){
+    $('#tit1').toggleClass('show')
+    $('#par1').toggle()
+});
+
+$('#tit2').click(function(){
+    $('#tit2').toggleClass('show')
+    $('#par2').toggle()
+});
+
+$('#tit3').click(function(){
+    $('#tit3').toggleClass('show')
+    $('#par3').toggle()
+});
+
+$('#tit4').click(function(){
+    $('#tit4').toggleClass('show')
+    $('#par4').toggle()
+});

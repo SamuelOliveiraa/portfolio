7766f9bd08ecfcab032d92007e88fc2cdc22e4ba
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2021-09-23 23:14:01 -0300

    Adicionei o toggle nas opcoes

--- Assets/script.js
+++ Assets/script.js
@@ -2,19 +2,53 @@
 $('#tit1').click(function(){
     $('#tit1').toggleClass('show')
     $('#par1').toggle()
+
+    remove('#tit2', '#par2')
+    remove('#tit3', '#par3')
+    remove('#tit4', '#par4')
+    remove('#tit5', '#par5')
 });
 
 $('#tit2').click(function(){
     $('#tit2').toggleClass('show')
     $('#par2').toggle()
+
+    remove('#tit1', '#par1')
+    remove('#tit3', '#par3')
+    remove('#tit4', '#par4')
+    remove('#tit5', '#par5')
 });
 
 $('#tit3').click(function(){
     $('#tit3').toggleClass('show')
     $('#par3').toggle()
+
+    remove('#tit2', '#par2')
+    remove('#tit1', '#par1')
+    remove('#tit4', '#par4')
+    remove('#tit5', '#par5')
 });
 
 $('#tit4').click(function(){
     $('#tit4').toggleClass('show')
     $('#par4').toggle()
+
+    remove('#tit2', '#par2')
+    remove('#tit3', '#par3')
+    remove('#tit1', '#par1')
+    remove('#tit5', '#par5')
 });
+$('#tit5').click(function(){
+    $('#tit5').toggleClass('show')
+    $('#par5').toggle()
+
+    remove('#tit2', '#par2')
+    remove('#tit3', '#par3')
+    remove('#tit4', '#par4')
+    remove('#tit1', '#par1')
+});
+
+function remove(idTitulo, idPar){
+    $(idTitulo).removeClass('show')
+    $(idPar).hide()
+}

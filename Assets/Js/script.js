edd7eba115c4202136031c9b8b1f35cec2df8852
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2021-10-09 23:37:20 -0300

    Arrumei bastante coisa
    Mas ainda falta algumas coisas para melhoras

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -12,6 +12,53 @@ $(document).ready(function () {
         $('header .icon').toggleClass("close")
         $('#nav').toggle()
     })
+
+    // TIRA E COLOCA A SOMBRA NO MENU
+    const header = document.querySelector('header')
+    const navHeight = header.offsetHeight
+
+    window.addEventListener('scroll', function() {
+        if(window.scrollY >= navHeight){
+            header.classList.add('scroll')
+        }else{
+            header.classList.remove('scroll')
+        }
+    })
+
+    /* ANIMAÇÃO DE MAQUINA DE ESCREVER (ORIGAMID) */
+    function typeWriter(elemento){
+        const textoArray = elemento.innerHTML.split('');
+        elemento.innerHTML = "";
+        textoArray.forEach((letra, i) =>{
+            setTimeout(() => elemento.innerHTML += letra, 50 * i)
+        });
+    }
+
+    const texto = document.getElementById('txt');
+
+    typeWriter(texto)
+
+    /* ROLAGEM SUAVE */
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+                targetOffset = $(id).offset().top;
+                
+        $('html, body').animate({ 
+            scrollTop: targetOffset - 100
+        }, 500);
+    });
+
+    $('.services .cards .card').hover(  
+          function(){
+
+                $(this).children('p').css('display', 'block')
+                $(this).css('height', '250px')
+            }, function() {
+                $(this).children('p').css('display', 'none')
+                $(this).css('height', '180px')
+            }
+    )
 })
 
 function condition(vpWidth) {

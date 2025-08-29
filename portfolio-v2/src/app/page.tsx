4b5903e24510e7419f89f89e678555b7add77287
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2025-08-29 18:44:18 -0300

    Inicializando o next.js

---
+++ portfolio-v2/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-gray-700 min-h-screen flex items-center justify-center text-white text-4xl">
+      Ola, mundo
+    </div>
+  );
+}

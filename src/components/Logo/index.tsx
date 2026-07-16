21d3f64d210021cc53af1dd15836b909bd681edd
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2026-07-16 09:19:57 -0300

    chore: Deleted all of old project

--- src/components/Logo/index.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Logo() {
-	return (
-		<Link href="/">
-			<h2 className="text-gray-900 text-xl md:text-4xl font-bold">{"<SO />"}</h2>
-		</Link>
-	);
-}

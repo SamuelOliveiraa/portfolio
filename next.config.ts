54c20f762b4866f28399d3414eaf1521a903bf90
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2026-07-19 18:55:37 -0300

    chore: Configured global and next config. Add react scroll and react icons to project.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["192.168.0.2", "localhost:3000"]
 };
 
 export default nextConfig;

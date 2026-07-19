8b1c763f050dff4eec9272b53999c15ed56d2a7a
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2026-07-19 19:02:03 -0300

    chore: Update header and hero section components

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,55 +1,50 @@
-import Link from "next/link";
-import Text from "../common/text";
+"use client";
+
 import ButtonComponent from "../common/button-component";
 import { ThemeToggle } from "../common/theme-toggle";
+import { Menu } from "lucide-react";
+import { LinkItem } from "../common/link-item";
+import Logo from "../common/logo";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const fn = () => setScrolled(window.scrollY > 30);
+    window.addEventListener("scroll", fn, { passive: true });
+    return () => window.removeEventListener("scroll", fn);
+  }, []);
+
   return (
-    <header className="max-w-6xl mx-auto py-4 flex justify-between items-center">
-      <Text size="textSmall" asChild className="uppercase">
-        <Link href="/">DEV.SAMUEL</Link>
-      </Text>
+    <header
+      id="header"
+      className={twMerge(
+        "w-full p-4 xl:px-0 sticky top-0 left-0 z-50 transition-all duration-300",
+        scrolled && "bg-zinc-50/90 backdrop-blur-md border-b"
+      )}
+    >
+      <div className="max-w-6xl w-full mx-auto flex justify-between items-center">
+        <Logo />
 
-      <nav>
-        <ul className="flex justify-between items-center gap-4 text-zinc-500 ">
-          <li>
-            <Text
-              size="textMedium"
-              className="hover:text-white transition-colors duration-200"
-            >
-              <Link href={"/services"}>Serviços</Link>
-            </Text>
-          </li>
-          <li>
-            <Text
-              size="textMedium"
-              className="hover:text-white transition-colors duration-200"
-            >
-              <Link href={"/services"}>Sobre</Link>
-            </Text>
-          </li>
-          <li>
-            <Text
-              size="textMedium"
-              className="hover:text-white transition-colors duration-200"
-            >
-              <Link href={"/services"}>Projetos</Link>
-            </Text>
-          </li>
-          <li>
-            <Text
-              size="textMedium"
-              className="hover:text-white transition-colors duration-200"
-            >
-              <Link href={"/services"}>Contato</Link>
-            </Text>
-          </li>
-        </ul>
-      </nav>
+        <nav className="hidden md:block">
+          <ul className="flex justify-between items-center gap-4 text-zinc-500 ">
+            <LinkItem link="services">Serviços</LinkItem>
+            <LinkItem link="about">Sobre</LinkItem>
+            <LinkItem link="projects">Projetos</LinkItem>
+            <LinkItem link="contact">Contato</LinkItem>
+          </ul>
+        </nav>
 
-      <div className="flex items-center gap-2">
-        <ThemeToggle />
-        <ButtonComponent className="w-fit">Fazer Orçamento</ButtonComponent>
+        <div className="hidden md:flex items-center gap-2">
+          <ThemeToggle />
+          <ButtonComponent className="w-fit">Fazer Orçamento</ButtonComponent>
+        </div>
+
+        <div className="block md:hidden">
+          <Menu />
+        </div>
       </div>
     </header>
   );
--- src/components/layout/hero-section.tsx
+++ src/components/layout/hero-section.tsx
@@ -1,26 +1,43 @@
 import { ArrowRight } from "lucide-react";
 import ButtonComponent from "../common/button-component";
 import Text from "../common/text";
+import { TypingAnimation } from "../ui/typing-animation";
+import MetricsComponent from "./metrics-component";
 
 export default function HeroSection() {
   return (
-    <section className="flex flex-col gap-6 max-w-6xl mx-auto mb-10">
-      <Text size="textSmall" className="text-emerald-100 uppercase">
-        Desenvolvedor Web Full-Stack
-      </Text>
+    <section
+      className="flex flex-col px-4 xl:px-0 gap-6 max-w-6xl mx-auto my-10 lg:mt-16"
+      id="/"
+    >
+      <div>
+        <Text size="textSmall" className="text-emerald-500 uppercase">
+          Desenvolvedor Web Full-Stack
+        </Text>
 
-      <Text size="titleLarge" as="h1" className="">
-        <span className="text-zinc-950 block">Desenvolvimento web</span>
-        sob medida que{" "}
-        <span className="text-emerald-100">gera resultados.</span>
-      </Text>
+        <Text size="titleLarge" as="h1" className="">
+          <span className="text-zinc-950 block">Desenvolvimento web</span>
+          sob medida que{" "}
+          <span className="text-emerald-900 block">
+            <TypingAnimation
+              className="leading-10 sm:leading-5"
+              words={[
+                "gera resultados.",
+                "vende mais.",
+                "atrai clientes.",
+                "escala seu negócio."
+              ]}
+            />
+          </span>
+        </Text>
+      </div>
 
       <Text size="textMedium" className="max-w-lg text-zinc-500">
         Do back-end ao front. Código que escala, entrega que não atrasa e
         comunicação que não deixa dúvidas.
       </Text>
 
-      <div className="flex flex-row gap-5">
+      <div className="flex flex-col sm:flex-row gap-5">
         <ButtonComponent className="w-fit">
           Fazer Orçamento
           <ArrowRight className="size-4 text-white" />
@@ -30,6 +47,8 @@ export default function HeroSection() {
           Ver Projetos
         </ButtonComponent>
       </div>
+
+      <MetricsComponent />
     </section>
   );
 }
